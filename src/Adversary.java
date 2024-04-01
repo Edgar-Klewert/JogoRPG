d55d6bc4ab5149ed117bd57e9f380049a40098a4
Edgar Klewert <127999109+Edgar-Klewert@users.noreply.github.com>
2024-04-01 17:00:26 -0300

    Update Adversary.java

--- src/Adversary.java
+++ src/Adversary.java
@@ -1,10 +1,10 @@
 class Adversary {
-    String nome;
-    int pv;
-    int dano;
-    int defesa;
-    int agilidade;
-    boolean defesaDobrada;
+    private String nome;
+    private int pv;
+    private int dano;
+    private int defesa;
+    private int agilidade;
+    private boolean defesaDobrada;
 
     public Adversary(String nome, int pv, int dano, int defesa, int agilidade) {
         this.nome = nome;
